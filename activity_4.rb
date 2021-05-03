f10e0b9c829929cf767c94d8c7d28fbb9d8fdb40
Eunicegenel <eunel_soliveres@yahoo.com>
2021-05-03 20:27:40 +0800

    edited file names

---
+++ activity_4.rb
@@ -0,0 +1,17 @@
+arr1 = [34,15,88,2]
+arr2 = [34, -345, -1, 100]
+
+def getLowest (arr)
+    lowest = arr[0]
+    x = 0
+    arr.each do |n|
+        if n < lowest
+            lowest = n
+        end
+    end
+    return p lowest
+end
+
+getLowest(arr1)
+getLowest(arr2)
+

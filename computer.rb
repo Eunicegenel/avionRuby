03abbc37b2a4eb15678f8f8e75f153921fea3723
Eunicegenel <eunel_soliveres@yahoo.com>
2021-05-03 20:07:57 +0800

    computer object

---
+++ computer.rb
@@ -0,0 +1,14 @@
+class Computer 
+    def initialize(cpu,gpu,motherboard)
+        @cpu = cpu
+        @gpu = gpu
+        @motherboard = motherboard
+    end
+
+    def benchmark
+        puts "Testing computer build of #{@cpu},  #{@gpu} and  #{@motherboard}"
+    end 
+end
+
+build1 = Computer.new("AMD Ryzen 5900", "GTX 3080", "Asus AM4")
+build1.benchmark
